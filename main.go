e0f5ce758a06def55364eab62437b306e209630e
mikehac <mike.hatskevich@gmail.com>
2022-12-05 23:29:49 +0200

    Creating web server

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"axiomsecurity/manifestlib"
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	manifestlib.InitComboBox()
 	message := manifestlib.Hello("Mike")
 	fmt.Println(message)
+
+	//Creating a web server
+	router := gin.Default()
+	router.GET("/form/:name", getFormManifest)
+	router.GET("/values/:source", getValues)
+	router.POST("/form/:name", postForm)
+
+	router.Run("localhost:3000")
+
 }
