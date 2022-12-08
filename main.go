b5036db73e1aaa65116c239400915e56e1ab8217
mikehac <mike_hac@outlook.com>
2022-12-08 13:37:42 +0200

    minor fixes

--- main.go
+++ main.go
@@ -32,20 +32,22 @@ func getFormManifest(c *gin.Context) {
 	formName := c.Param("name")
 	if currentManifest, ok := manifestlib.ManifestsMap[formName]; ok {
 		c.JSON(http.StatusOK, currentManifest)
+	} else {
+		c.JSON(http.StatusNoContent, gin.H{
+			"code":    http.StatusNoContent,
+			"message": string("No manifest named '" + formName + "' was found")})
 	}
-	c.JSON(http.StatusNoContent, gin.H{
-		"code":    http.StatusNoContent,
-		"message": string("No manifest named '" + formName + "' was found")})
 }
 
 func getValues(c *gin.Context) {
 	source := c.Param("source")
 	if currentSource, responseStatus := manifestlib.SourceValueMap[source]; responseStatus {
 		c.JSON(http.StatusOK, currentSource)
+	} else {
+		c.JSON(http.StatusNoContent, gin.H{
+			"code":    http.StatusNoContent,
+			"message": string("No source named '" + source + "' was found")})
 	}
-	c.JSON(http.StatusNoContent, gin.H{
-		"code":    http.StatusNoContent,
-		"message": string("No source named '" + source + "' was found")})
 }
 
 func postForm(c *gin.Context) {
