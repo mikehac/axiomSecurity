3d4ece38aefded6a569b09ea5cf276eb8680f1f1
mikehac <mike_hac@outlook.com>
2022-12-08 12:28:15 +0200

    finishing handler

--- manifestLib/formDataModel.go
+++ manifestLib/formDataModel.go
@@ -1,18 +1,18 @@
 package manifestlib
 
 type DatabaseForm struct {
-	Reason            *string `json:"reason"`
-	DatabaseInstances *string `json:"databaseInstances"`
-	DatabaseRole      *string `json:"databaseRole"`
+	Reason            *string `json:"Reason"`
+	DatabaseInstances *string `json:"DatabaseInstances"`
+	DatabaseRole      *string `json:"DatabaseRole"`
 }
 
 type CloudForm struct {
-	Reason   *string `json:"reason"`
-	Accounts *string `json:"accounts"`
-	User     *string `json:"user"`
+	Reason   *string `json:"Reason"`
+	Accounts *string `json:"Accounts"`
+	User     *string `json:"User"`
 }
 
 type Validator interface {
 	isValidType(sourceJsonStr []byte) bool
-	valuesAreValid() bool
+	valuesAreValid(manifestName string) bool
 }
--- manifestLib/validationHandler.go
+++ manifestLib/validationHandler.go
@@ -3,6 +3,8 @@ package manifestlib
 import (
 	"encoding/json"
 	"fmt"
+
+	"golang.org/x/exp/slices"
 )
 
 func (d *DatabaseForm) isValidType(sourceJsonStr []byte) bool {
@@ -14,17 +16,63 @@ func (c *CloudForm) isValidType(sourceJsonStr []byte) bool {
 	err := json.Unmarshal([]byte(sourceJsonStr), &c)
 	return err == nil && c.Reason != nil && c.Accounts != nil && c.User != nil
 }
-func (d *DatabaseForm) valuesAreValid() bool{
+func (d *DatabaseForm) valuesAreValid() bool {
+	//Reflection approach
+	// field := reflect.Indirect(reflect.ValueOf(d))
+	// for i := 0; i < field.Type().NumField(); i++ {
+	// 	currentFieldName := field.Type().Field(i).Name
+	// 	possibleValues := SourceValueMap[currentFieldName]
+	// 	containsValue := slices.Contains(possibleValues.Values, *d.DatabaseInstances)
+	// 	if !containsValue && currentFieldName != "Reason" {
+	// 		fmt.Println("values don't match acording the manifest")
+	// 		// return false
+	// 	}
+	// 	fmt.Println(possibleValues.Values)
+	// }
+	// r := reflect.ValueOf(*d)
+	// values := make([]interface{}, r.NumField())
+	// for i := 0; i < r.NumField(); i++ {
+	// 	// currentFieldName := field.Type().Field(i).Name
+	// 	// possibleValues := SourceValueMap[currentFieldName]
+	// 	// currentValue := r.Field(i).Interface()
+	// 	// fmt.Println(currentValue)
+	// 	// containsValue := slices.Contains(possibleValues.Values, string(&currentValue))
+	// 	// if !containsValue && currentFieldName != "Reason" {
+	// 	// 	fmt.Println("values don't match acording the manifest")
+	// 	// 	return false
+	// 	// }
+	// 	values[i] = r.Field(i).Interface()
+	// 	fmt.Println(values[i])
+	// 	fmt.Println(&values[i])
+	// }
+	// fmt.Println(&values)
+	databaseInstancesValues := SourceValueMap["DatabaseInstances"]
+	databaseRoleValues := SourceValueMap["DatabaseRole"]
+	//Because the Reason is a string and the required validation is to check if it is not empty
+	//there is no need to check the value against the SourceValueMap
+	if *d.Reason != "" &&
+		slices.Contains(databaseInstancesValues.Values, *d.DatabaseInstances) &&
+		slices.Contains(databaseRoleValues.Values, *d.DatabaseRole) {
+		return true
+	}
+
 	return false
 }
 
-func (c *CloudForm) valuesAreValid() bool  {
+func (c *CloudForm) valuesAreValid() bool {
+	accountsValues := SourceValueMap["Accounts"]
+	userValues := SourceValueMap["User"]
+	if *c.Reason != "" &&
+		slices.Contains(accountsValues.Values, *c.Accounts) &&
+		slices.Contains(userValues.Values, *c.User) {
+		return true
+	}
+
 	return false
 }
-func ValidateTypeAndValues(sorceJsonStr []byte, manifestName string) bool{
+func ValidateTypeAndValues(sorceJsonStr []byte, manifestName string) bool {
 	var d DatabaseForm
 	var c CloudForm
-
 	// items := [...]Validator{&d, &c}
 	// fmt.Println(items)
 	// for _, item := range items {
@@ -47,6 +95,6 @@ func ValidateTypeAndValues(sorceJsonStr []byte, manifestName string) bool{
 			return true
 		}
 	}
-	
+
 	return false
 }
